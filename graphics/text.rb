ce309af69ab591aea6ac7da90a58bf3517101240
David Harper <daharper@hotmail.com>
2022-07-30 16:17:29 +0100

    Update text.rb

--- graphics/text.rb
+++ graphics/text.rb
@@ -3,13 +3,12 @@ class Text
     canvas.check(x1,y1)
     raise "missing value for message error" if message.empty?
 
-    x = x1
     msg = message.chars
 
     loop do
-      canvas.draw(x,y1,msg.shift)
-      x += 1
-      break if x > canvas.width or msg.empty?
+      canvas.draw(x1,y1,msg.shift)
+      x1 += 1
+      break if x1 > canvas.width or msg.empty?
     end
   end
 end
